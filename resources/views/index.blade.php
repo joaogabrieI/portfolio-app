e2b90c46e0311622b9532ab8fd9b8514420859c3
joaogabrieI[ <joaonalves1@gmail.com>
2024-02-20 14:29:23 -0300

    esboço do html

---
+++ resources/views/index.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portifólio</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#about_me">Sobre mim</a></li>
+                <li><a href="#technologies">Tecnologias</a></li>
+                <li><a href="#academic">Currículo acadêmico</a></li>
+                <li><a href="#projects">Projetos</a></li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="#contact-me">Contatos</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section id="about_me">
+            <div class="main_msg">
+                <h1>Olá, eu sou João Gabriel, um desenvolvedor de software.</h1>
+                <p>Sou um estudante dedicado de Desenvolvimento de Software com entusiasmo por aprender e crescer na indústria. Minhas principais áreas de interesse são o desenvolvimento de aplicações com linguagem PHP, HTML, CSS, Javascript e SQL. Estou constantemente aprimorando minhas habilidades técnicas e buscando oportunidades para aplicar meus conhecimentos em projetos práticos.</p>
+                <a href="#contact_me">Entre em contato</a>
+            </div>
+            <img src="../images/eu.jpg" alt="">
+        </section>
+        <section id="technologies">
+            <div>
+                <img src="" alt="">
+                <h2>PHP</h2>
+                <p>descrição do que sei</p>
+            </div>
+            <div>
+                <h2>Javascript</h2>
+                <img src="" alt="">
+                <p>descrição do que sei</p>
+            </div>
+            <div>
+                <img src="" alt="">
+                <h2>Java</h2>
+                <p>descrição do que sei</p>
+            </div>
+            <div>
+                <img src="" alt="">
+                <h2>HTML</h2>
+                <p>descrição do que sei</p>
+            </div>
+            <div>
+                <img src="" alt="">
+                <h2>CSS</h2>
+                <p>descrição do que sei</p>
+            </div>
+            <div>
+                <img src="" alt="">
+                <h2>SQL</h2>
+                <p>descrição do que sei</p>
+            </div>
+        </section>
+        <section id="academic">
+            <div>
+                <h2>2022 - atualmente</h2>
+                <p>Serviço Nacional de Aprendizagem Comercial - SENAC</p>
+                <p>Técnologo em Sistemas para internet</p>
+            </div>
+            <div>
+                <h2>2020-2021</h2>
+                <p>Centro Paula Souza - Escola Técnica Estadual de São Paulo</p>
+                <p>Técnico em desenvolvimento de sistemas</p>
+            </div>
+        </section>
+        <section id="projects">
+            <div>
+                <img src="" alt="">
+                <h2>Landing page FT Máquinas</h2>
+                <p>Página de divulgação desenvolvida para a empresa FT Máquinas</p>
+            </div>
+
+            <div>
+                <img src="" alt="">
+                <h2>Crud Alpha</h2>
+                <p>Aplicação para administração de produtos de uma loja de jogos.</p>
+            </div>
+        </section>
+    </main>
+    <footer id="contact_me">
+        <h2>Informações para contato</h2>
+        <p>joaonalves1@gmail</p>
+        <p>linkedin</p>
+    </footer>
+</body>
+
+</html>
